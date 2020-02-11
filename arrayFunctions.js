640c64de38de9771ca6f2800a4b54157ab8a3466
sebaBocchioOguic <60882728+sebaBocchioOguic@users.noreply.github.com>
2020-02-11 20:39:54 -0300

    Update arrayFunctions.js

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -6,3 +6,25 @@ function simpleArraySum(ar) {
     return sum;
 }
 
+--------------------------------------
+
+// Compare 2 arrays[3] and returns the result of bigger by index [2,1]
+function compareTriplets(a, b) {
+
+    // Create array
+    var comparision = [0,0];
+
+    // Loop for comparision
+    for (var i=0; i<3; i++){
+        if (a[i]>b[i]) {comparision[0]++;}
+        else if (a[i]<b[i]) {comparision[1]++;}
+    }
+
+    // Return values
+    return comparision;
+
+}
+
+--------------------------------------
+
+
