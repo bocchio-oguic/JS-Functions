0d354864196bde16c167801560e9b02a0be5d2ef
sebaBocchioOguic <60882728+sebaBocchioOguic@users.noreply.github.com>
2021-01-30 08:36:28 -0300

    Update arrayFunctions.js

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -6,26 +6,7 @@ function simpleArraySum(ar) {
     return sum;
 }
 
---------------------------------------
-
-// Compare 2 arrays[3] and returns the result of bigger by index [2,1]
-function compareTriplets(a, b) {
-
-    // Create array
-    var comparision = [0,0];
-
-    // Loop for comparision
-    for (var i=0; i<3; i++){
-        if (a[i]>b[i]) {comparision[0]++;}
-        else if (a[i]<b[i]) {comparision[1]++;}
-    }
-
-    // Return values
-    return comparision;
-
-}
-
---------------------------------------
+-----------------------------
 
 // Calculation of absolute difference between the sum of diagonals inside a matrix
 function diagonalDifference(arr) {
